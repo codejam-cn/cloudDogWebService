abc5f7376a04d6ccbd8993379199b0a2b088764a
festone000 <festone000@hotmail.com>
2016-12-06 23:57:19 +0800

    create a new branch named "develop_code_first". In order to manage this project using the code first tech.

--- cdws/Edmx/CloudDog.Designer.cs
+++ cdws/Edmx/CloudDog.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\git\cloudDogWebService\cdws\cdws\Edmx\CloudDog.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\My\cloudDogWebService\cdws\Edmx\CloudDog.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
